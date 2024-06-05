e098e5f0174d936f309d4c5f73e629fa570c1ce4
ssielinou <steve.sielinou.ext@sisal.it>
2024-06-05 17:40:44 +0200

    Initial commit

---
+++ src/main/java/com/infoevent/joparis/JoParisApplication.java
@@ -0,0 +1,13 @@
+package com.infoevent.joparis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoParisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoParisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/infoevent/joparis/JoParisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoevent.joparis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoParisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
